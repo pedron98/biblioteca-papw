ad99e9cd0107e670ef0b44b8fb8999f0dbacdb25
Pedro Augusto <pedro98_augusto@hotmail.com>
2019-12-12 05:45:04 -0300

    Update AccountCredentialsVO and Migrations

--- src/main/java/br/com/pedro/jwt/AccountCredentialsVO.java
+++ src/main/java/br/com/pedro/jwt/AccountCredentialsVO.java
@@ -1,7 +1,11 @@
 package br.com.pedro.jwt;
 
-public class AccountCredentialsVO {
+import java.io.Serializable;
 
+public class AccountCredentialsVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 	private String userName;
 	private String password;
 	
